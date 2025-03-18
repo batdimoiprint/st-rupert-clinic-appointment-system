29ee4f2ec7b08330ba392158ed5177a3402f7dd2
sean <mixeuseo@gmail.com>
2025-03-19 03:17:23 +0800

    New components: Headers (for users and admin).

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header> 
+        <div class = "logo">
+            <img src = "../assets/clinic-logo.svg" alt = "St. Rupert Medical Clinic logo">
+            <span>
+                <h1>St. Rupert</h1>
+                <p>Medical Clinic</p>
+            </span>
+        </div>
+        <nav>
+            <ul>
+                <li><a href = "#">HOME</a></li>
+                <li><a href = "#">OUR SERVICES</a></li>
+                <li><a href = "#">FAQs</a></li>
+                <li><a href = "#">ABOUT US</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+
+</script>
+
+<style lang = "scss">
+@include header;
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/variables.scss" as *; @use "@/styles/mixins.scss" as *;`
+      },
+    },
+  },
+});
